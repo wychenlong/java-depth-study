c8ce17ea325f148ae2cdc81d416b602f21bd27c1
wychenlong <cl315917525@gmail.com>
2016-07-12 22:07:17 +0800

    添加guava排序

--- src/main/java/com/wanying/study/guava/basic/OrderingDemo.java
+++ src/main/java/com/wanying/study/guava/basic/OrderingDemo.java
@@ -17,14 +17,21 @@ public class OrderingDemo {
         Comparator natural = Ordering.natural();
         //自然排序
         Collections.sort(strList,natural);
-        System.out.println(strList);
+        System.out.println("自然排序："+strList);
         //倒排
         Comparator reverse = Ordering.natural().reverse();
         Collections.sort(strList,reverse);
-        System.out.println(strList);
+        System.out.println("自然倒排："+strList);
 
+        List<Integer> numberList = Arrays.asList(6,61,28,12,45);
         //求最小元素、最大元素
-        System.out.println();
+        System.out.println("最大："+Ordering.natural().max(numberList));
+        System.out.println("最小："+Ordering.natural().min(numberList));
+
+//      比较顺序
+        System.out.println("比较顺序："+Ordering.explicit(numberList));
+
+
 
     }
 }
