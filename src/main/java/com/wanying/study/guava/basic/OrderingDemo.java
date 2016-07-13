2ec9e88ff68c2f301675b0d4a8061ebcf7ad0c92
unknown <wychenlong@360buyAD.local>
2016-07-13 20:29:58 +0800

    增加guava Ordering方法学习

--- src/main/java/com/wanying/study/guava/basic/OrderingDemo.java
+++ src/main/java/com/wanying/study/guava/basic/OrderingDemo.java
@@ -1,11 +1,11 @@
 package com.wanying.study.guava.basic;
 
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Ordering;
+import com.wanying.study.guava.basic.vo.Foo;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by cl on 2016/7/9.
@@ -19,6 +19,7 @@ public class OrderingDemo {
         Collections.sort(strList,natural);
         System.out.println("自然排序："+strList);
         //倒排
+//      流式风格
         Comparator reverse = Ordering.natural().reverse();
         Collections.sort(strList,reverse);
         System.out.println("自然倒排："+strList);
@@ -31,7 +32,29 @@ public class OrderingDemo {
 //      比较顺序
         System.out.println("比较顺序："+Ordering.explicit(numberList));
 
+//      复制copy新集合
+        System.out.println("排序复制到新集合："+Ordering.natural().sortedCopy(numberList));
+//      倒序后取多少个元素
+        System.out.println("greatestOf："+Ordering.natural().greatestOf(numberList,3));
+//      自然排序后取满足指定多少个元素
+        System.out.println("leastOf："+Ordering.natural().leastOf(numberList,3));
 
+//        复杂对象排序支持
+        Random random = new Random();
+        List<Foo> fooList = Lists.newArrayList();
+        for(int i = 0 ; i < 5 ;i++){
+            Foo foo = new Foo();
+            foo.setAge(random.nextInt(40));
+            fooList.add(foo);
+        }
+        System.out.println("原始数据：" +fooList);
+        Ordering<Foo> orderingFoo = Ordering.natural().nullsFirst().onResultOf(new Function<Foo, Comparable>() {
+            @Override
+            public Comparable apply(Foo foo) {
+                return foo.getAge();
+            }
+        }).nullsFirst();
+        System.out.println("对象字段排序：" +orderingFoo.sortedCopy(fooList));
 
     }
 }
---
+++ src/main/java/com/wanying/study/guava/basic/vo/Foo.java
@@ -0,0 +1,23 @@
+package com.wanying.study.guava.basic.vo;
+
+/**
+ * Created by wychenlong on 2016/7/13.
+ */
+public class Foo {
+    Integer age;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Foo{" +
+                "age=" + age +
+                '}';
+    }
+}
