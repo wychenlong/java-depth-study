eb5ab5e50e88c48b602f89008735aa9aaa8e6d77
unknown <wychenlong@360buyAD.local>
2016-07-22 18:27:39 +0800

    添加hash学习

--- src/main/java/com/wanying/study/guava/hash/HashDemo.java
+++ src/main/java/com/wanying/study/guava/hash/HashDemo.java
@@ -5,8 +5,7 @@ package com.wanying.study.guava.hash;
  */
 public class HashDemo {
     public static void main(String args[]){
-        System.out.println("eee".hashCode());
-        System.out.println(Integer.valueOf("11111").hashCode());
+
 
     }
 }
--- src/main/java/com/wanying/study/guava/vo/Person.java
+++ src/main/java/com/wanying/study/guava/vo/Person.java
@@ -4,4 +4,41 @@ package com.wanying.study.guava.vo;
  * Created by cl on 2016/7/17.
  */
 public class Person {
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (!name.equals(person.name)) return false;
+        return age.equals(person.age);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + age.hashCode();
+        return result;
+    }
 }
